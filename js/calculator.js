101ada1b13cb69b02280e08e738330057a9c9973
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-02-13 01:23:28 +0600

    Complete

--- js/calculator.js
+++ js/calculator.js
@@ -2,7 +2,7 @@ const valueField = document.getElementById('input');
 function numbers(btnId, textToAdd) {
     const valueField = document.getElementById('input')
     const button7 = document.getElementById(btnId).addEventListener('click', function() {
-        if(valueField.innerText == '0') {
+        if(valueField.innerText == '') {
             valueField.innerText = textToAdd;
         }
         else{
@@ -22,32 +22,109 @@ const button3 = numbers('btn-3', '3');
 const button0 = numbers('btn-0', '0');
 const buttonDot = numbers('btn-.', '.');
 
-const previousValue = document.getElementById('input');
-const previousValueText = previousValue.innerText;
-const previousValueAmount = parseFloat(previousValueText);
-console.log(previousValueAmount);
-    // const newId = document.getElementById('input')
-    // const newIdText = newId.innerText;
-    // const newIdValue = parseFloat(newIdText);
-    // const total = prevIdValue + newIdValue;
-    // console.log(total);
-    
+const operator = document.getElementById('operator');
+const ac = document.getElementById('btn-ac').addEventListener('click', function(event) {
+    reload = location.reload();
+})
+function getPreviousValue() {
+    const previousValue = document.getElementById('input');
+    const previousValueText = previousValue.innerText;
+    const previousValueAmount = parseFloat(previousValueText);
+    console.log(previousValueAmount);
+    previousValue.innerText = '';
+    return previousValueAmount;
+}
 
-const ac = document.getElementById('btn-ac').addEventListener('click', function() {
-    previousValue.innerText = '0';
-})
 const del = document.getElementById('btn-del').addEventListener('click', function() {
-    previousValue.innerText = valueAfaterDelete;
+    const deleteText = document.getElementById('input').innerText;
+    document.getElementById('input').innerText = deleteText.substring(0, deleteText.length -1);
 })
+
 const plus = document.getElementById('btn-+').addEventListener('click', function() {
-    // previousValue.innerText = '0';
+    operator.innerText = '+';
+    operator.classList.add('text-white');
+    operator.classList.remove('text-dark');
+
+    const previousValueAmount = getPreviousValue();
     
-    const equal = document.getElementById('btn-=').addEventListener('click', function() {
-        const newId = document.getElementById('input');
-        const newIdText = newId.innerText;
-        const newIdValue = parseFloat(newIdText);
-        console.log(newIdValue);
-        const total = previousValueAmount + newIdValue;
-        valueField.innerText = total;
+    document.getElementById('btn-=').addEventListener('click', function() {
+        const newValue = document.getElementById('input');
+        const newValueText = newValue.innerText;
+        const newValueAmount = parseFloat(newValueText);
+        console.log(newValueAmount);
+        const total = previousValueAmount + newValueAmount;
+        newValue.innerText = total;
+        operator.innerText = previousValueAmount + ' + ' + newValueAmount;
     })
 })
+
+const minus = document.getElementById('btn-').addEventListener('click', function() {
+    operator.innerText = '-';
+    operator.classList.add('text-white');
+    operator.classList.remove('text-dark');
+
+    const previousValueAmount = getPreviousValue();
+    
+    document.getElementById('btn-=').addEventListener('click', function() {
+        const newValue = document.getElementById('input');
+        const newValueText = newValue.innerText;
+        const newValueAmount = parseFloat(newValueText);
+        console.log(newValueAmount);
+        const total = previousValueAmount - newValueAmount;
+        newValue.innerText = total;
+        operator.innerText = previousValueAmount + ' - ' + newValueAmount;
+    })
+})
+const multiplication = document.getElementById('btn-*').addEventListener('click', function() {
+    operator.innerText = 'X';
+    operator.classList.add('text-white');
+    operator.classList.remove('text-dark');
+
+    const previousValueAmount = getPreviousValue();
+    
+    document.getElementById('btn-=').addEventListener('click', function() {
+        const newValue = document.getElementById('input');
+        const newValueText = newValue.innerText;
+        const newValueAmount = parseFloat(newValueText);
+        console.log(newValueAmount);
+        const total = previousValueAmount * newValueAmount;
+        newValue.innerText = total;
+        operator.innerText = previousValueAmount + ' X ' + newValueAmount;
+    })
+})
+const division = document.getElementById('btn-/').addEventListener('click', function() {
+    operator.innerText = '/';
+    operator.classList.add('text-white');
+    operator.classList.remove('text-dark');
+
+    const previousValueAmount = getPreviousValue();
+    
+    document.getElementById('btn-=').addEventListener('click', function() {
+        const newValue = document.getElementById('input');
+        const newValueText = newValue.innerText;
+        const newValueAmount = parseFloat(newValueText);
+        console.log(newValueAmount);
+        const total = previousValueAmount / newValueAmount;
+        newValue.innerText = total;
+        operator.innerText = previousValueAmount + ' / ' + newValueAmount;
+    })
+})
+
+const percentage = document.getElementById('btn-%').addEventListener('click', function() {
+    operator.innerText = '%';
+    operator.classList.add('text-white');
+    operator.classList.remove('text-dark');
+
+    const previousValueAmount = getPreviousValue();
+    
+    document.getElementById('btn-=').addEventListener('click', function() {
+        const newValue = document.getElementById('input');
+        const newValueText = newValue.innerText;
+        const newValueAmount = parseFloat(newValueText);
+        console.log(newValueAmount);
+        const total = (previousValueAmount / 100) * newValueAmount;
+        newValue.innerText = total;
+        operator.innerText = previousValueAmount + ' % ' + newValueAmount;
+    })
+})
+
